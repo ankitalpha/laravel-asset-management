7576347502682b9bb2c15bbf854b5bfdb61b20c8
Ankit Tiwari <ankit.tiwari@drivezy.com>
2019-08-29 09:52:22 +0530

    development #comment add country, model, controller and migration

---
+++ app/Models/Country.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drivezy\LaravelAssetManager\Models;
+
+use Drivezy\LaravelUtility\Models\BaseModel;
+use Drivezy\LaravelAssetManager\Observers\CountryObserver;
+/**
+ * Class Country
+ * @package Drivezy\LaravelAssetManager
+ *
+ * @see https://github.com/drivezy/laravel-asset-manager.
+ * @author Ankit Tiwari <ankit19.alpha@gmail.com>
+ */
+class Country extends BaseModel {
+
+    /**
+     * @var string
+     * The Country table name.
+     */
+    protected $table = 'dz_countries';
+
+    /**
+     * Boot observer.
+     */
+    public static function boot () {
+        parent::boot();
+        self::observe(new CountryObserver());
+    }
+}
---
+++ app/Observers/CountryObserver.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drivezy\LaravelAssetManager\Observers;
+
+use Drivezy\LaravelUtility\Observers\BaseObserver;
+
+/**
+ * Class Country
+ * @package Drivezy\LaravelAssetManager
+ *
+ * @see https://github.com/drivezy/laravel-asset-manager.
+ * @author Ankit Tiwari <ankit19.alpha@gmail.com>
+ */
+class CountryObserver extends BaseObserver {
+
+    /**
+     * @var array Required parameters.
+     */
+    protected $rules = [
+    ];
+}
